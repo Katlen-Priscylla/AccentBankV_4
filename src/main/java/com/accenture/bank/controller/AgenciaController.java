1ba65c00cd90e9d9441ae1169e926ff4b5c33c88
Katlen-Priscylla <katlen.priscylla@gmail.com>
2021-07-24 22:46:08 -0300

    Agencia

--- src/main/java/com/accenture/bank/controller/AgenciaController.java
+++ src/main/java/com/accenture/bank/controller/AgenciaController.java
@@ -1,27 +1,52 @@
 package com.accenture.bank.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.bank.entity.Agencia;
+import com.accenture.bank.entity.Agencia;
 import com.accenture.bank.servico.AgenciaService;
 
 @RestController
-//@RequestMapping("/agencias")
+@RequestMapping("agencias/")
 public class AgenciaController {
 	@Autowired
 	AgenciaService agenciaService;
 	
 	
-	@RequestMapping(value = "/agencias", method =  RequestMethod.POST)
+	
 	@PostMapping
 	public Agencia save( @Valid @RequestBody Agencia agencia) {
 		return agenciaService.saveAgencia(agencia);
 	}
+	
+	@GetMapping
+	public List<Agencia> findAll (){
+		return agenciaService.getAllAgencias();
+	}
+	@GetMapping("{id}")
+	public Agencia findId(@PathVariable Long id) {
+		return agenciaService.getById(id);
+	}
+	@PutMapping("{id}")
+	public Agencia atualizaAgencia(@PathVariable Long id, @RequestBody Agencia agencia ) {
+		return agenciaService.atualizaAgencia(id, agencia);
+	}
+	@DeleteMapping("{id}")
+	public void deletaById(@PathVariable Long id) {
+		agenciaService.deletaAgencia(id);
+	}
+	
 }
--- src/main/java/com/accenture/bank/servico/AgenciaService.java
+++ src/main/java/com/accenture/bank/servico/AgenciaService.java
@@ -1,11 +1,14 @@
 package com.accenture.bank.servico;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.accenture.bank.entity.Agencia;
+import com.accenture.bank.entity.Agencia;
 import com.accenture.bank.repository.AgenciaRepository;
-import com.accenture.bank.repository.ContaCorrenteRepository;
+import com.accenture.bank.repository.AgenciaRepository;
 //import com.accenture.bank.repository.ContaRepository;
 import com.accenture.bank.repository.EnderecoRepository;
 
@@ -16,10 +19,36 @@ public class AgenciaService {
 //	@Autowired
 //	EnderecoRepository enderecoRepository;
 	@Autowired
-	ContaCorrenteRepository contaCorrenteRepository;
-	
+	AgenciaRepository contaCorrenteRepository;
+
 	public Agencia saveAgencia(Agencia agencia) {
-		contaCorrenteRepository.saveAll(agencia.getContas());
+		// contaCorrenteRepository.saveAll(agencia.getContas());
 		return agenciaRepository.save(agencia);
+	}
+
+	public List<Agencia> getAllAgencias() {
+
+		return agenciaRepository.findAll();
+
+	}
+
+	public Agencia getById(Long id) {
+		return agenciaRepository.findById(id).get();
+	}
+
+	public Agencia atualizaAgencia(long id, Agencia newAgencia) {
+		newAgencia.setIdAgencia(id);
+		Agencia oldAgencia = agenciaRepository.findById(id).get();
+		if (oldAgencia != null) {
+			oldAgencia.setNome(newAgencia.getNome());
+			oldAgencia.setEndereco(newAgencia.getEndereco());
+			newAgencia = oldAgencia;
+			return agenciaRepository.save(newAgencia);
+		} else
+			return null;
+	}
+
+	public void deletaAgencia(Long id) {
+		agenciaRepository.deleteById(id);
 	}
 }
