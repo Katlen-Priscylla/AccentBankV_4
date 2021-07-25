0c98f89b14a35bb42b6bc5d5804fb85cc161e32b
Katlen-Priscylla <katlen.priscylla@gmail.com>
2021-07-24 22:27:44 -0300

    Conta_Corrente
    Conta corrente ok

--- src/main/java/com/accenture/bank/controller/ContaCorrenteController.java
+++ src/main/java/com/accenture/bank/controller/ContaCorrenteController.java
@@ -1,24 +1,51 @@
 package com.accenture.bank.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.bank.entity.ContaCorrente;
+import com.accenture.bank.entity.ContaCorrente;
+import com.accenture.bank.servico.ContaCorrenteService;
 import com.accenture.bank.servico.ContaCorrenteService;
 
 @RestController
+@RequestMapping("contas/")
 public class ContaCorrenteController {
-
 	@Autowired
 	ContaCorrenteService contaCorrenteService;
 	
-	@RequestMapping(value = "/contas", method =  RequestMethod.POST)
-	public ContaCorrente save( @Valid @RequestBody ContaCorrente contaCorrente) {
+	@PostMapping 
+	public ContaCorrente save(@RequestBody ContaCorrente contaCorrente) {
 		return contaCorrenteService.saveConta(contaCorrente);
 	}
+	
+	@GetMapping
+	public List<ContaCorrente> findAll (){
+		return contaCorrenteService.getAllContaCorrente();
+	}
+	@GetMapping("{id}")
+	public ContaCorrente findId(@PathVariable Long id) {
+		return contaCorrenteService.getByIdContaCorrente(id);
+	}
+	@PutMapping("{id}")
+	public ContaCorrente atualizaContaCorrente(@PathVariable Long id, @RequestBody ContaCorrente contaCorrente ) {
+		return contaCorrenteService.atualizaContaCorrente(id, contaCorrente);
+	}
+	@DeleteMapping("{id}")
+	public void deletaById(@PathVariable Long id) {
+		contaCorrenteService.deletaContaCorrente(id);
+	}
+	
 }
--- src/main/java/com/accenture/bank/servico/ContaCorrenteService.java
+++ src/main/java/com/accenture/bank/servico/ContaCorrenteService.java
@@ -1,8 +1,11 @@
 package com.accenture.bank.servico;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.accenture.bank.entity.Cliente;
 import com.accenture.bank.entity.ContaCorrente;
 import com.accenture.bank.repository.AgenciaRepository;
 import com.accenture.bank.repository.ClienteRepository;
@@ -11,20 +14,44 @@ import com.accenture.bank.repository.ContaCorrenteRepository;
 @Service
 public class ContaCorrenteService {
 
-	@Autowired
-	AgenciaRepository agenciaRepository;
+//	@Autowired
+//	AgenciaRepository agenciaRepository;
 	@Autowired
 	ContaCorrenteRepository contaCorrenteRepository;
-	@Autowired
-	ClienteRepository clienteRepository;
-	
-	public ContaCorrente saveConta(ContaCorrente contaCorrente)
-	{
+//	@Autowired
+//	ClienteRepository clienteRepository;
+
+	public ContaCorrente saveConta(ContaCorrente contaCorrente) {
 //		agenciaRepository.save(contaCorrente.getAgencia());
 //		clienteRepository.save(contaCorrente.getCliente());
-		
+
 		return contaCorrenteRepository.save(contaCorrente);
-		
+
 	}
-	
+
+	public List<ContaCorrente> getAllContaCorrente() {
+
+		return contaCorrenteRepository.findAll();
+
+	}
+
+	public ContaCorrente getByIdContaCorrente(Long id) {
+		return contaCorrenteRepository.findById(id).get();
+	}
+
+	public ContaCorrente atualizaContaCorrente(long id, ContaCorrente newContaCorrente) {
+		newContaCorrente.setIdContaCorrente(id);
+		ContaCorrente oldContaCorrente = contaCorrenteRepository.findById(id).get();
+		if (oldContaCorrente != null) {
+			
+			oldContaCorrente.setValor(newContaCorrente.getValor());
+			newContaCorrente = oldContaCorrente;
+			return contaCorrenteRepository.save(newContaCorrente);
+		} else
+			return null;
+	}
+
+	public void deletaContaCorrente(Long id) {
+		contaCorrenteRepository.deleteById(id);
+	}
 }
