67878678dbc87dd663ff48ff800501583589f73d
Katlen-Priscylla <katlen.priscylla@gmail.com>
2021-07-27 23:45:22 -0300

    Update ClienteServiceTest.java

--- src/test/java/com/accenture/bank/servico/ClienteServiceTest.java
+++ src/test/java/com/accenture/bank/servico/ClienteServiceTest.java
@@ -1,23 +1,5 @@
 package com.accenture.bank.servico;
 
-
-import java.util.Optional;
-
-import org.aspectj.lang.annotation.Before;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-
-import com.accenture.bank.entity.Cliente;
-import com.accenture.bank.entity.Endereco;
-import com.accenture.bank.repository.ClienteRepository;
-
-
 public class ClienteServiceTest {
-	
-	
+
 }
