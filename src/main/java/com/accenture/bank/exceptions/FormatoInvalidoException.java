7b9dcd421a3edb7b414f48a1148ea97b5d54a58e
Katlen-Priscylla <katlen.priscylla@gmail.com>
2021-07-29 17:07:38 -0300

    Create FormatoInvalidoException.java
    exceção do formato

---
+++ src/main/java/com/accenture/bank/exceptions/FormatoInvalidoException.java
@@ -0,0 +1,13 @@
+package com.accenture.bank.exceptions;
+
+public class FormatoInvalidoException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FormatoInvalidoException(){
+		super("formato Invalido");
+	}
+}
+
